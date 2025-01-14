223130b731dae7810d98da743ae5bc8ea039803b
heejae369 <heejae369@gmail.com>
2025-01-15 01:17:53 +0900

    admin controller 구현

--- src/main/java/controller/AdminController.java
+++ src/main/java/controller/AdminController.java
@@ -1,4 +1,13 @@
 package controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AdminController {
+
+    @GetMapping("/admin")
+    public String AdminPage(){
+        return "admin";
+    }
 }
